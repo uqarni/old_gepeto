5f73552f3677584efe09fac2b247b62dc03167e3
uqarni <108438834+uqarni@users.noreply.github.com>
2023-02-23 22:32:27 -0500

    functions.py
    cleaner done
    uploader WIP
    file_status NS
    fine_tuner NS
    model_status NS
    query_model NS

---
+++ functions.py
@@ -0,0 +1,57 @@
+import pandas as pd
+
+def cleaner(df):
+    #take df and return jsonl file
+    jsonlz = ""
+    for index,row in df.iterrows():
+        raw_data = row["raw text"]
+
+        for index,i in enumerate(raw_data):
+            if i == "{":
+                start = index
+            elif i == "}":
+                end = index
+                jsonlz = jsonlz + raw_data[start:(end+1)]
+                jsonlz = jsonlz + "\n"
+
+    return jsonlz
+
+# df = pd.read_csv('training_data_5.csv')
+# only_jsonls = cleaner(df)
+# final_training_data = open("cleansed_training_data_2.jsonl", "x")
+# final_training_data.write(only_jsonls)
+# print(final_training_data.read())
+
+
+def uploader(prompt_responses_file_path):
+    #upload it to openai
+    file_name = prompt_responses_file_path
+    import openai
+
+    apikey = "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"
+    openai.api_key = apikey
+
+    send = openai.File.create(
+        file=open(file_name, "rb"),
+        purpose='fine-tune'
+    )
+
+    return send["id"]
+
+test = uploader("cleansed_training_data_2.jsonl")
+    
+
+def file_status(file_id):
+    status = 'foo'
+    return status
+
+def fine_tuner(file_id, model):
+    task_id = "foo"
+    return task_id
+
+def model_status(task_id):
+    model_id = "foo"
+    return model_id
+
+def query_model(model_id, prompt, tokens = 200):
+    return 0
